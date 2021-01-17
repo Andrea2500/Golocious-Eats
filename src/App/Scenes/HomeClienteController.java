6c90b10269abcb78161ffbaa84c17f046c795b73
Marcello Russo <marcellorusso.x@gmail.com>
2021-01-17 13:01:52 +0100

    spostati controller con i file fxml e aggiunta possibilità di cambiare scenes tramite metodi in LoginController, HomeClienteController e SceneController

--- src/App/Scenes/HomeClienteController.java
+++ src/App/Scenes/HomeClienteController.java
@@ -7,7 +7,7 @@ public class HomeClienteController extends Controller {
 
     SceneController sceneController = new SceneController();
 
-    public void backBtn(ActionEvent e) throws Exception{
+    public void backBtn(ActionEvent e) throws Exception {
         this.sceneController.setLogin(e);
     }
 
