db7e57a859ab8c9c0972721d71cd31ff0accfaa2
Marcello Russo <marcellorusso.x@gmail.com>
2021-02-09 14:45:07 +0100

    rinominazione commit

--- src/App/Scenes/Controller/ConsegnaController.java
+++ src/App/Scenes/Controller/ConsegnaController.java
@@ -41,35 +41,42 @@ public class ConsegnaController extends BaseSceneController implements Initializ
 
     private void showConsegneAttive() {
         this.consegnaVBox.getChildren().clear();
-        for (Ordine consegna: consegneAttive){
-            HBox hBox = new HBox();
-            VBox vBox1 = new VBox();
-            VBox vBox2 = new VBox();
-            Label ristorante = new Label("Ristorante di ritiro: "+consegna.getRistorante().toString());
-            Label indirizzo = new Label("Indirizzo di consegna: "+consegna.getIndirizzo().toString());
-            Label nomeCliente =  new Label("Nome cliente: "+consegna.getNomeCliente());
-            Label telefonoCliente = new Label("Telefono cliente: "+consegna.getTelefonoCliente());
-            Button consegnaBtn = new Button("Termina consegna");
-            consegnaBtn.setOnAction(actionEvent -> {
-                try {
-                    this.effettuaConsegnaController.consegna(consegna.getOrdineId());
-                    this.effettuaConsegnaController.aggiornaConsegne();
-                    this.consegneAttive.clear();
-                    this.consegneAttive = this.effettuaConsegnaController.getConsegneAttive();
-                    this.showConsegneAttive();
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-            });
-            vBox1.alignmentProperty().set(Pos.CENTER_LEFT);
-            vBox2.alignmentProperty().set(Pos.CENTER_LEFT);
-            hBox.alignmentProperty().set(Pos.CENTER_LEFT);
-            vBox2.setStyle("-fx-pref-width: 245px");
-            hBox.getStyleClass().add("elementoConsegna");
-            vBox1.getChildren().addAll(ristorante, indirizzo);
-            vBox2.getChildren().addAll(nomeCliente, telefonoCliente);
-            hBox.getChildren().addAll(vBox1, vBox2, consegnaBtn);
-            this.consegnaVBox.getChildren().add(hBox);
+        if(consegneAttive.size()>0){
+            for (Ordine consegna : consegneAttive) {
+                HBox hBox = new HBox();
+                VBox vBox1 = new VBox();
+                VBox vBox2 = new VBox();
+                Label ristorante = new Label("Ristorante di ritiro: " + consegna.getRistorante().toString());
+                Label indirizzo = new Label("Indirizzo di consegna: " + consegna.getIndirizzo().toString());
+                Label nomeCliente = new Label("Nome cliente: " + consegna.getNomeCliente());
+                Label telefonoCliente = new Label("Telefono cliente: " + consegna.getTelefonoCliente());
+                Button consegnaBtn = new Button("Termina consegna");
+                consegnaBtn.setOnAction(actionEvent -> {
+                    try {
+                        this.effettuaConsegnaController.consegna(consegna.getOrdineId());
+                        this.effettuaConsegnaController.aggiornaConsegne();
+                        this.consegneAttive.clear();
+                        this.consegneAttive = this.effettuaConsegnaController.getConsegneAttive();
+                        this.showConsegneAttive();
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+                });
+                vBox1.alignmentProperty().set(Pos.CENTER_LEFT);
+                vBox2.alignmentProperty().set(Pos.CENTER_LEFT);
+                hBox.alignmentProperty().set(Pos.CENTER_LEFT);
+                vBox2.setStyle("-fx-pref-width: 245px");
+                hBox.getStyleClass().add("elementoConsegna");
+                vBox1.getChildren().addAll(ristorante, indirizzo);
+                vBox2.getChildren().addAll(nomeCliente, telefonoCliente);
+                hBox.getChildren().addAll(vBox1, vBox2, consegnaBtn);
+                this.consegnaVBox.getChildren().add(hBox);
+            }
+        } else {
+            consegnaVBox.setAlignment(Pos.CENTER);
+            Label nessunaConsegna = new Label("Non hai consegne attive al momento");
+            nessunaConsegna.setStyle("-fx-font-size: 16px");
+            this.consegnaVBox.getChildren().add(nessunaConsegna);
         }
     }
 
